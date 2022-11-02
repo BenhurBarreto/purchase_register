1d43db2bfb9e484dd008068a06066bb08940b7cc
Benhur <benhurbbs@gmail.com>
2022-11-01 21:03:56 -0300

    Adding .idea file

--- qrCodeProject.py
+++ qrCodeProject.py
@@ -20,6 +20,7 @@ while True:
         print(myData)
         pts = numpy.array([barcode.polygon], numpy.int32)
         pts = pts.reshape((-1, 1, 2))
+        pts = pts.r((-1, 1, 2))
         cv2.polylines(img, [pts], True, (255, 0, 255), 5)
         pts2 = barcode.rect
         cv2.putText(img, myData, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 255), 2)
