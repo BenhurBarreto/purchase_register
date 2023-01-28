aa03cb2fc72086de9a41f30d58b7eb8e3f747e8f
BenhurBarreto <benhurbbs@gmail.com>
2023-01-28 19:17:13 -0300

    Modificação do while para limitar as tentativas de leitura do qrcode

--- qrCodeProject.py
+++ qrCodeProject.py
@@ -11,19 +11,33 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-while True:
+tentativas = 0
+myNewData = ''
+myData = ''
+
+while tentativas < 5:
 
     success, img = cap.read()
     for barcode in decode(img):
         # print(barcode.data)
+        # print(myNewData)
         myData = barcode.data.decode('utf-8')
-        print(myData)
+        # print(myData)
+
         pts = numpy.array([barcode.polygon], numpy.int32)
         pts = pts.reshape((-1, 1, 2))
-        pts = pts.r((-1, 1, 2))
+
+        # pts = pts.r((-1, 1, 2))
         cv2.polylines(img, [pts], True, (255, 0, 255), 5)
         pts2 = barcode.rect
         cv2.putText(img, myData, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 255), 2)
+        
+        if myNewData == myData:
+            tentativas += 1
+            print(myData + ' ' + str(tentativas))
+            
+        myNewData = myData
 
     cv2.imshow('Result', img)
     cv2.waitKey(1)
+    # print('foi ate aqui')
